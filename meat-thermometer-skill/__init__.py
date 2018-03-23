ca36da5b101f2cf7076466e0fc84ee35e68755b6
Andrew <ajgriesemer@gmail.com>
2018-03-22 22:12:40 -0500

    Modifying template to respond to temperature prompt.

--- meat-thermometer-skill/__init__.py
+++ meat-thermometer-skill/__init__.py
@@ -38,34 +38,15 @@ class MeatThermometerSkill(MycroftSkill):
 
     # The constructor of the skill, which calls MycroftSkill's constructor
     def __init__(self):
-        self.count = 0
         super(MeatThermometerSkill, self).__init__(name="MeatThermometerSkill")
 
-    # The "handle_xxxx_intent" function is triggered by Mycroft when the
-    # skill's intent is matched.  The intent is defined by the IntentBuilder()
-    # pieces, and is triggered when the user's utterance matches the pattern
-    # defined by the keywords.  In this case, the match occurs when one word
-    # is found from each of the files:
-    #    vocab/en-us/Hello.voc
-    #    vocab/en-us/World.voc
-    # In this example that means it would match on utterances like:
-    #   'Hello world'
-    #   'Howdy you great big world'
-    #   'Greetings planet earth'
-    @intent_handler(IntentBuilder("").require("Hello").require("World"))
-    def handle_hello_world_intent(self, message):
-        # In this case, respond by simply speaking a canned response.
-        # Mycroft will randomly speak one of the lines from the file
-        #    dialogs/en-us/hello.world.dialog
-        self.speak_dialog("hello.world")
-
-    @intent_handler(IntentBuilder("").require("Count").require("Dir"))
+    @intent_handler(IntentBuilder("").require("Temperature").require("Meat"))
     def handle_count_intent(self, message):
-        if message.data["Dir"] == "up":
-            self.count += 1
+        if message.data["Temperature"] == "turkey":
+            temperature = 175
         else:  # assume "down"
-            self.count -= 1
-        self.speak_dialog("cooking.temperature.is", data={"count": self.count})
+            temperature = 165
+        self.speak_dialog("cooking.temperature.is", data={"temperature": temperature})
 
     # The "stop" method defines what Mycroft does when told to stop during
     # the skill's execution. In this case, since the skill's functionality
